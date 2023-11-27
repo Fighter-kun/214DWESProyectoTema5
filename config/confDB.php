0f94715867882d17b744f37f0b731f0bdc64fd38
Fighter-kun <carlosgarciacachon@gmail.com>
2023-11-27 19:29:50 +0100

    UPDATE - .gitignore actualizado, incluyo el confDB de Desarrollo

--- config/confDB.php
+++ config/confDB.php
@@ -4,17 +4,18 @@
  * @version 1.0 
  * @since 08/11/2023
  * Archivo de configuración de la BD del Instituto
- 
+ */
 define('DSN', 'mysql:host=192.168.20.19;dbname=DB214DWESProyectoTema5'); // Host 'IP' y nombre de la base de datos
 define('USERNAME','user214DWESProyectoTema5'); // Nombre de usuario de la base de datos
 define('PASSWORD','paso'); // Contraseña de la base de datos
-*/
+
 /**
  * @author Carlos García Cachón
  * @version 1.0 
  * @since 26/11/2023
  * Archivo de configuración de la BD de Explotación
- */
+
 define('DSN', 'mysql:host=db5014806801.hosting-data.io;dbname=dbs12302455'); // Host y nombre de la base de datos
 define('USERNAME','dbu132588'); // Nombre de usuario de la base de datos
 define('PASSWORD','daw2_Sauces'); // Contraseña de la base de datos
+ */
